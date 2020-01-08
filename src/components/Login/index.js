cf6b01375251b388f4b1d12e5485acea7656ba65
Patrick Kennedy <pkennedy2@SF-US05643-MBP13.local>
2020-01-07 16:44:06 -0800

    added final touches

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -8,7 +8,10 @@ class Login extends React.Component {
         return(
             <div style={{maxWidth: '400px', margin: '0 auto'}}> 
                 <Form>
-                    <h1>Login</h1>
+                    <br />
+                    <br />
+                    <h1 style={{textAlign: 'center'}}>Event Signin Login</h1>
+                    <br />
                     <Row>
                         <Col>
                             <Button onClick={this.props.signInWithGoogle} variant='primary' block>
@@ -17,9 +20,22 @@ class Login extends React.Component {
                         </Col>
                     </Row>
                     <br />
+                    <hr />
                     <Row>
                         <Col>
-                            <h6>Need Instructions? <span>😇</span></h6>
+                            <h3>Instructions</h3>
+                            <br />
+                            <h6>1. Login with your <b>//Flatiron</b> email.</h6>
+                            <br />
+                            <h6>2. Create a new event.</h6>
+                            <br />
+                            <h6>3. You will be asked to verify a google account that has <b>write access</b> to the google sheet.</h6>
+                            <br />
+                            <h6>4. Use the link below to go to the sheet and ask for permission if you don't already.</h6>
+                            <br />
+                            <h6>5. Enjoy your event <span>🥳</span></h6>
+                            <br />
+                            <b><a target='_blank' href='https://docs.google.com/spreadsheets/d/1bJqnfZGA1x1r5ZDE9Lb5yQl9_0c4A3Fy3h5hMPtOr-c/edit#gid=0'><span>📑</span> Event Signin Sheet</a></b>
                         </Col>
                     </Row>
                 </Form>
